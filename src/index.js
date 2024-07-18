4527f5dd2a5cc1ad46f58b8c92ae4eda9e69ca36
terenceouma <doros.devs@gmail.com>
2024-07-18 16:18:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  const imageContainer = document.getElementById('dog-image-container');
+  const breedsList = document.getElementById('dog-breeds');
+  const breedDropdown = document.getElementById('breed-dropdown');
+  let allBreeds = [];
+
+  // Challenge 1
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+      data.message.forEach(imageUrl => {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        imageContainer.appendChild(img);
+      });
+    });
+
+  // Challenge 2
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => {
+      allBreeds = Object.keys(data.message);
+      renderBreeds(allBreeds);
+    });
+
+  function renderBreeds(breeds) {
+    breedsList.innerHTML = '';
+    breeds.forEach(breed => {
+      const li = document.createElement('li');
+      li.innerText = breed;
+      li.addEventListener('click', () => {
+        li.style.color = 'firebrick'; // Change this color to whatever you prefer
+      });
+      breedsList.appendChild(li);
+    });
+  }
+
+  // Challenge 3
+  // Included in the renderBreeds function above
+
+  // Challenge 4
+  breedDropdown.addEventListener('change', (event) => {
+    const selectedLetter = event.target.value;
+    const filteredBreeds = allBreeds.filter(breed => breed.startsWith(selectedLetter));
+    renderBreeds(filteredBreeds);
+  });
+});
